8b91a0af24a0534e5c10a1c96f60fa82ae52b37e
Bartłomiej Postek <68648068+BarPos@users.noreply.github.com>
2024-06-02 17:38:33 +0200

    Update

--- src/index.h
+++ src/index.h
@@ -9,14 +9,114 @@ static std::string indexFile = R"html(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>srvstats</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+        *{
+            font-family: "Roboto", sans-serif;
+            font-weight: 300;
+            font-style: normal;
+        }
+        body{
+            background: #2c3e50;
+            color: #bdc3c7;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            width: 100vw;
+        }
+        .box{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            width: 100%;
+            max-width: 48rem;
+            border: 1px #34495e solid;
+            border-radius: 1rem;
+        }
+        .data{
+            width: calc(100% - 2rem);
+
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+        .data-left{
+            text-align: right;
+            width: 92px;
+        }
+        .data-right{
+            width: 42px;
+        }
+        .data-center{
+            width: calc(48rem - 2rem - 92px - 42px - 2rem);
+            display: block;
+            width: max-content;
+        }
+        .data-center div{
+            height: 20px;
+            margin-top: 16px;
+            margin-bottom: 16px;
+            width: calc(48rem - 2rem - 92px - 42px - 2rem);
+            max-width: calc(100vw - 2rem - 92px - 42px - 2rem);
+            border-radius: 1rem;
+            background: #95a5a6;
+            overflow: hidden;
+        }
+        .data-center div div{
+            height: 100%;
+            transform: translateY(-16px);
+            background: #3498db;
+        }
+        .data1{
+            width: calc(100% - 2rem);
+
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+        .data1-left{
+            text-align: right;
+            width: 80px;
+        }
+        .data1-right{
+            width: calc(48rem - 2rem - 92px - 1rem);
+            max-width: calc(100vw - 2rem - 92px - 1rem);
+        }
+    </style>
 </head>
 <body>
-    <h1>srvstats</h1>
-    <p>Uptime: <span class="_uptime">Loading...</span></p>
-    <p>CPU Cores: <span class="_cores">Loading...</span></p>
-    <p>CPU Ussage: <span class="_cpu">0</span>%</p>
-    <p>Memory: <span class="_memory">0</span>%</p>
-    <p>Swap: <span class="_swap">0</span>%</p>
+    <div class="box">
+        <h1>srvstats <small>v0.1</small></h1>
+        <div class="data1">
+            <div class="data1-left">
+                <p>Uptime:</p>
+                <p>CPU Cores:</p>
+            </div>
+            <div class=" data1-right">
+                <p><span class="_uptime">Loading...</span></p>
+                <p><span class="_cores">Loading...</span></p>
+            </div>
+        </div>
+        <div class="data">
+            <div class="data-left">
+                <p>CPU Ussage:</p>
+                <p>Memory:</p>
+                <p>Swap:</p>
+            </div>
+            <div class="data-center">
+                <div><div class="_cpu_" style="width: 0%;"></div></div>
+                <div><div class="_memory_" style="width: 0%;"></div></div>
+                <div><div class="_swap_" style="width: 0%;"></div></div>
+            </div>
+            <div class="data-right">
+                <p><span class="_cpu">0</span>%</p>
+                <p><span class="_memory">0</span>%</p>
+                <p><span class="_swap">0</span>%</p>
+            </div>
+        </div>
+    </div>
 
     <script>
         const uptime = document.querySelector("._uptime")
@@ -24,6 +124,10 @@ static std::string indexFile = R"html(
         const cpu = document.querySelector("._cpu")
         const memory = document.querySelector("._memory")
         const swap = document.querySelector("._swap")
+
+        const _cpu = document.querySelector("._cpu_")
+        const _memory = document.querySelector("._memory_")
+        const _swap = document.querySelector("._swap_")
 
         const update = async () =>{
             var data = await fetch("/data")
@@ -34,6 +138,10 @@ static std::string indexFile = R"html(
             cpu.innerHTML = Math.round(d.cpu * 100) / 100
             memory.innerHTML = Math.round(d.memory * 100) / 100
             swap.innerHTML = Math.round(d.swap * 100) / 100
+
+            _cpu.style.width = d.cpu + "%"
+            _memory.style.width = d.memory + "%"
+            _swap.style.width = d.swap + "%"
         }
         update()
         setInterval(async () => {
