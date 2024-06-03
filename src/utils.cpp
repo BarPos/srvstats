ff0532fd07d69e1d586c350f9888d2431ed89732
Bartłomiej Postek <68648068+BarPos@users.noreply.github.com>
2024-06-03 21:15:33 +0200

    Update

--- src/utils.cpp
+++ src/utils.cpp
@@ -75,7 +75,7 @@ void startCacheThread() {
 		while (true) {
 			CPU_stats t1 = read_cpu_data();
 
-			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
 			CPU_stats t2 = read_cpu_data();
 			ussageCache = (100.0f * get_cpu_usage(t1, t2));
